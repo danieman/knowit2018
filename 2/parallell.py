c8d872602c33c36f77b086c45e6364c0502b35d8
danieman <34152459+danieman@users.noreply.github.com>
2018-12-03 00:12:42 +0100

    Tabs --> spaces

--- 2/parallell.py
+++ 2/parallell.py
@@ -3,8 +3,8 @@ import re
 d = {}
 
 for line in open('input.txt'):
-	x1, y1, x2, y2 = map(int, re.findall(r'\d+', line.strip()))
-	s = (y2-y1)/(x2-x1)
-	d[s] = d.get(s, 0) + 1
+    x1, y1, x2, y2 = map(int, re.findall(r'\d+', line.strip()))
+    s = (y2-y1)/(x2-x1)
+    d[s] = d.get(s, 0) + 1
 
 print(max(d.values()))
