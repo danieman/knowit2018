ffb55cefb635f51e10143109c0e701c10fdbeec6
danieman <34152459+danieman@users.noreply.github.com>
2018-12-03 03:40:32 +0100

    Slå sammen to linjer kode til én

--- 1/vekksort.py
+++ 1/vekksort.py
@@ -1,9 +1,8 @@
-with open('input.txt') as f:
-    total, min = 0, 0
-    for line in f:
-        num = int(line.strip())
-        if num >= min: 
-            total += num
-            min = num
+total, min = 0, 0
+for line in open('input.txt'):
+    num = int(line.strip())
+    if num >= min: 
+        total += num
+        min = num
 
 print(total)
